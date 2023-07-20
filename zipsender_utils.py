ac23cc74ebd1d921edd943fd8258700b8847e92a
apenasrr <apenasrr@gmail.com>
2023-07-20 19:22:36 -0300

    Open txt by context manager

--- zipsender_utils.py
+++ zipsender_utils.py
@@ -10,7 +10,6 @@ import zipfile
 
 
 def load_json(json_path):
-
     list_encode = ["utf-8-sig", "utf16", "ISO-8859-1"]  # utf8, utf16, ansi
     for encode in list_encode:
         try:
@@ -28,14 +27,12 @@ def load_json(json_path):
 
 
 def create_txt(file_path, stringa):
-
     file = open(file_path, "w", encoding="utf8")
     file.write(stringa)
     file.close()
 
 
 def save_txt(str_content, str_name):
-
     # UTF-8 can't handle with the follow caracter in a folder name: 
     text_file = open(f"{str_name}.txt", "w", encoding="utf_16")
     text_file.write(str_content)
@@ -52,7 +49,6 @@ def extzip_haspwd(file_path):
 
 
 def zip_haspwd(file_path):
-
     dot_extension = os.path.splitext(file_path)[1]
     extension = dot_extension.strip(".")
     if extension == "rar":
@@ -87,7 +83,6 @@ def get_size_group(list_, size_max):
 
 
 def split_list_in_lists(list_, size_group):
-
     list_of_list = []
     list_cache = []
     for i in list_:
@@ -101,7 +96,6 @@ def split_list_in_lists(list_, size_group):
 
 
 def split_list_in_lists_by_max(list_, size_max):
-
     size_group = get_size_group(list_, size_max)
     list_of_list = split_list_in_lists(list_, size_group)
     return list_of_list
@@ -115,7 +109,6 @@ def add_log(status, msg):
 
 
 def add_path_script_folders(list_folders_name):
-
     list_repo_dont_found = []
     for folder_name in list_folders_name:
         path_script_folder = os.path.abspath(os.path.join("..", folder_name))
@@ -150,7 +143,6 @@ def get_list_folder_to_zip_ready(list_folder_to_zip):
 
 
 def get_folder_to_zip_ready(list_folder_to_zip_ready):
-
     if len(list_folder_to_zip_ready) == 0:
         return False
     else:
@@ -158,7 +150,6 @@ def get_folder_to_zip_ready(list_folder_to_zip_ready):
 
 
 def compile_template(d_keys, template_content):
-
     for key in d_keys.keys():
         template_content = template_content.replace(
             "{" + key + "}", d_keys[key]
@@ -168,18 +159,15 @@ def compile_template(d_keys, template_content):
 
 
 def get_txt_content(file_path):
-
     list_encode = ["utf-8", "ISO-8859-1"]  # utf8, ansi
     for encode in list_encode:
         try:
-            file = open(file_path, "r", encoding=encode)
-            file_content = file.readlines()
-            file_content = "".join(file_content)
-            file.close()
-            return file_content
+            with open(file_path, "r", encoding=encode) as file:
+                file_content = file.read()
+                return file_content
         except:
             continue
 
-    file = open(file_path, "r", encoding=encode)
-    file_content = file.readlines()
+    with open(file_path, "r", encoding=encode) as file:
+        file_content = file.read()
     raise Exception("encode", f"Cannot open file: {file_path}")
