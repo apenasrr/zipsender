6950cbb48cda8b21924d2d996011028d02a6daec
apenasrr <apenasrr@gmail.com>
2023-07-20 19:54:21 -0300

    Flag delete_after_zip
    Flag delete_after_zip to delete folder project after zipping

--- zipsender_zip.py
+++ zipsender_zip.py
@@ -10,12 +10,10 @@ from zipind import zipind_core, zipind_utils
 import content_tree
 import extra
 import zipsender_utils
-from tree_directory import get_tree_directory
-from zipsender_utils import add_path_script_folders, zip_haspwd
+from zipsender_utils import zip_haspwd
 
 
 def get_list_folder_to_zip_ready(list_folder_to_zip):
-
     list_ = []
     for folder_to_zip in list_folder_to_zip:
         if folder_to_zip[0] == "_":
@@ -24,7 +22,6 @@ def get_list_folder_to_zip_ready(list_folder_to_zip):
 
 
 def get_folder_to_zip_approved(list_folders_path_approved):
-
     if len(list_folders_path_approved) == 0:
         return False
     else:
@@ -34,47 +31,41 @@ def get_folder_to_zip_approved(list_folders_path_approved):
 
 
 def sanitize_folder(folder_path):
-
     folder_path_raw = zipind_utils.normalize_string_to_link(folder_path)
     folder_path_sanitize = folder_path_raw.strip("_")
 
     return folder_path_sanitize
 
 
-def get_path_dir_output(folder_zipped, folder_path_input):
-
+def get_folder_path_output(folder_zipped, folder_path_input):
     folder_project = sanitize_folder(folder_path=folder_path_input)
-    path_dir_output = Path(folder_zipped) / folder_project
-    zipind_utils.ensure_folder_existence([path_dir_output])
-    path_dir_output = Path(folder_zipped) / folder_project / "output"
-    zipind_utils.ensure_folder_existence([path_dir_output])
-    return path_dir_output
+    folder_path_output = Path(folder_zipped) / folder_project
+    zipind_utils.ensure_folder_existence([folder_path_output])
+    folder_path_output = Path(folder_zipped) / folder_project / "output"
+    zipind_utils.ensure_folder_existence([folder_path_output])
+    return folder_path_output
 
 
-def get_path_dir_project_toupload_auth(path_dir_project_toupload):
-
-    name_dir_project = os.path.basename(path_dir_project_toupload)
+def get_folder_path_project_toupload_auth(folder_path_project_toupload):
+    folder_name_project = os.path.basename(folder_path_project_toupload)
 
     # auth caracter
-    name_dir_project_auth = "_" + name_dir_project
+    folder_name_project_auth = "_" + folder_name_project
 
     folder_toupload = os.path.abspath(
-        os.path.join(path_dir_project_toupload, "..")
+        os.path.join(folder_path_project_toupload, "..")
     )
     project_toupload_auth = os.path.join(
-        folder_toupload, name_dir_project_auth
+        folder_toupload, folder_name_project_auth
     )
 
     return project_toupload_auth
 
 
-def set_project_toupload_auth(path_dir_output):
-
-    path_dir_project_toupload = os.path.abspath(
-        os.path.join(path_dir_output, "..")
-    )
-    path_dir_project_toupload_auth = get_path_dir_project_toupload_auth(
-        path_dir_project_toupload
+def set_project_toupload_auth(folder_path_output: Path):
+    folder_path_project_toupload = folder_path_output.parent.absolute()
+    folder_path_project_toupload_auth = get_folder_path_project_toupload_auth(
+        folder_path_project_toupload
     )
 
     msg_rename_fail = (
@@ -86,7 +77,7 @@ def set_project_toupload_auth(path_dir_output):
     while True:
         try:
             os.rename(
-                path_dir_project_toupload, path_dir_project_toupload_auth
+                folder_path_project_toupload, folder_path_project_toupload_auth
             )
             if alert_flag == 1:
                 print(msg_rename_ok)
@@ -101,17 +92,15 @@ def set_project_toupload_auth(path_dir_output):
                 time.sleep(2)
 
 
-def get_list_path_folder(path_folder_root, list_folder_name):
-
-    list_path_folder = []
+def get_list_folder_path(folder_path_root, list_folder_name):
+    list_folder_path = []
     for folder_name in list_folder_name:
-        path_folder = os.path.join(path_folder_root, folder_name)
-        list_path_folder.append(path_folder)
-    return list_path_folder
+        folder_path = os.path.join(folder_path_root, folder_name)
+        list_folder_path.append(folder_path)
+    return list_folder_path
 
 
 def set_unauth_folder(folder_path):
-
     folder_name = os.path.basename(folder_path)
     folder_path_root_draft = os.path.join(folder_path, "..")
     folder_path_root = os.path.abspath(folder_path_root_draft)
@@ -125,14 +114,12 @@ def set_unauth_folder(folder_path):
 
 
 def set_unauth_folders(list_folders_path_rejected):
-
     if len(list_folders_path_rejected) > 0:
         for folders_path_rejected in list_folders_path_rejected:
             set_unauth_folder(folder_path=folders_path_rejected)
 
 
 def ensure_folders_sanatize(list_folders_path_rejected):
-
     if len(list_folders_path_rejected) > 0:
         # remove authorization character from rejected folders
         set_unauth_folders(list_folders_path_rejected)
@@ -164,18 +151,16 @@ def revert_original_folder_name(folder_path: str) -> bool:
         return False
 
 
-def get_folder_structure(path_folder):
-
-    list_path_file = []
-    for root, _, files in os.walk(path_folder):
+def get_folder_structure(folder_path):
+    list_file_path = []
+    for root, _, files in os.walk(folder_path):
         for file in files:
-            path_file = os.path.join(root, file)
-            list_path_file.append(path_file)
-    return list_path_file
+            file_path = os.path.join(root, file)
+            list_file_path.append(file_path)
+    return list_file_path
 
 
 def save_log_folder_structure(folder_name, dict_tasks, log_folder_path):
-
     dict_tasks_log = copy.deepcopy(dict_tasks)
     folder_name_sanitize = sanitize_folder(folder_name)
     log_file_path = get_log_file_path(folder_name_sanitize, log_folder_path)
@@ -185,14 +170,12 @@ def save_log_folder_structure(folder_name, dict_tasks, log_folder_path):
 
 
 def get_log_file_path(folder_name, log_folder_path):
-
     folder_destiny = log_folder_path
     log_file_path = os.path.join(folder_destiny, folder_name)
     return log_file_path
 
 
 def format_list_file_path(list_file_path):
-
     list_file_path_formatted = []
     for file_path in list_file_path:
         deep_level = file_path.count("\\")
@@ -204,18 +187,16 @@ def format_list_file_path(list_file_path):
 
 
 def test_zipfiles_in_one_folder(folder_path):
-    def get_list_zip_file_path(list_path_file):
-
+    def get_list_zip_file_path(list_file_path):
         list_zip_file_path = []
-        for path_file in list_path_file:
-            dot_extension = os.path.splitext(path_file)[1]
+        for file_path in list_file_path:
+            dot_extension = os.path.splitext(file_path)[1]
             extension = dot_extension.strip(".")
             if extension in ["zip", "rar", "7z"]:
-                list_zip_file_path.append(path_file)
+                list_zip_file_path.append(file_path)
         return list_zip_file_path
 
     def get_list_file_with_pwd(list_zip_file_path):
-
         list_file_with_pwd = []
         for zip_file_path in list_zip_file_path:
             haspwd = zip_haspwd(zip_file_path)
@@ -243,7 +224,6 @@ def test_zipfiles_in_one_folder(folder_path):
 
 
 def test_zipfiles(list_folders_path_approved):
-
     for folder_path in list_folders_path_approved:
         if test_zipfiles_in_one_folder(folder_path) is False:
             return False
@@ -253,7 +233,6 @@ def test_zipfiles(list_folders_path_approved):
 def move_config_to_parent_folder_path_output(
     folder_path_input: Path, folder_path_output: Path
 ) -> None:
-
     config_path_orgn = Path(folder_path_input) / ".config"
     config_path_dest = Path(folder_path_output).parent.absolute() / ".config"
     if config_path_orgn.exists():
@@ -261,7 +240,6 @@ def move_config_to_parent_folder_path_output(
 
 
 def main():
-
     config = ConfigParser()
     config.read("config.ini")
     default_config = dict(config["default"])
@@ -274,6 +252,7 @@ def main():
     max_path = int(default_config.get("max_path"))
     mode = default_config.get("mode")
     log_folder_path = default_config.get("log_folder_path")
+    delete_after_zip = int(default_config.get("delete_after_zip"))
 
     zipind_utils.ensure_folder_existence(
         [folder_tozip, folder_zipped, folder_toupload, log_folder_path]
@@ -285,8 +264,8 @@ def main():
         list_folder_to_zip_ready = get_list_folder_to_zip_ready(
             list_folder_to_zip
         )
-        list_path_folder_to_zip_ready = get_list_path_folder(
-            path_folder_root=folder_tozip,
+        list_folder_path_to_zip_ready = get_list_folder_path(
+            folder_path_root=folder_tozip,
             list_folder_name=list_folder_to_zip_ready,
         )
 
@@ -295,7 +274,7 @@ def main():
             list_folders_path_approved,
             list_folders_path_rejected,
         ) = zipind_utils.test_folders_has_path_too_long(
-            list_path_folder_to_zip_ready, max_path
+            list_folder_path_to_zip_ready, max_path
         )
 
         # ensures that authorized folders are approved in the path_max test
@@ -319,7 +298,7 @@ def main():
         folder_input_name = Path(folder_path_input).name
 
         # define destination folder.: toupload
-        folder_path_output = get_path_dir_output(
+        folder_path_output = get_folder_path_output(
             folder_toupload, folder_input_name
         )
 
@@ -334,6 +313,7 @@ def main():
         )
 
         # Creates grouped files for independent compression
+        # project description include as caption in read_more post
         dict_tasks = zipind_core.get_dict_tasks(
             folder_path_input, mb_per_file, folder_path_output, mode
         )
@@ -345,13 +325,17 @@ def main():
         # Start Compression
         zipind_core.zipind_process(dict_tasks, mode)
 
-        time.sleep(4)
-
         # move from original folder to zipped
+        time.sleep(3)
         shutil.move(folder_path_input, folder_zipped)
 
+        if delete_after_zip == 1:
+            # remove project folder after zip
+            time.sleep(3)
+            shutil.rmtree(str(Path(folder_zipped) / folder_input_name))
+
         # rename zipped project folder, adding authorization character
-        set_project_toupload_auth(folder_path_output)
+        set_project_toupload_auth(Path(folder_path_output))
 
         print("Zip finished: ", folder_input_name)
 
