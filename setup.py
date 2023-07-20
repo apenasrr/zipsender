c827be8102aefd4f5957884ca9a56c10372922be
apenasrr <apenasrr@gmail.com>
2023-07-20 20:11:22 -0300

    v0.1.12-Multiple channels sending and new flags
    Feature: direct sending to multiple channels
    Flag delete_after_send to delete zipped project after sending
    Flag delete_after_zip to delete folder project after zipping
    Add config flag time_limit 50

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='zipsender',
-    version='0.0.2',
-    description='Workflow for splitting, zipping and uploading folders',
-    url='https://github.com/apenasrr/zipsender',
-    author='apenasrr',
-    author_email='apenasrr@gmail.com',
+    name="zipsender",
+    version="0.1.12",
+    description="Workflow for splitting, zipping and uploading folders",
+    url="https://github.com/apenasrr/zipsender",
+    author="apenasrr",
+    author_email="apenasrr@gmail.com",
     packages=[],
-    classifiers=['Development Status :: 1 - Planning'],
+    classifiers=["Development Status :: 4 - Beta"],
 )
